ef57b8bf5559b6e9062cf54330ff26ce5ce17cda
twinbird <ixa2063@gmail.com>
2022-05-14 01:21:40 +0000

    Add keyword "int" and make variable definition mandatory

--- chibicc.h
+++ chibicc.h
@@ -28,6 +28,7 @@ typedef struct Obj Obj;
 struct Obj {
   Obj *next;
   char *name; // Variable name
+  Type *ty;   // Type
   int offset; // Offset from RBP
 };
 
@@ -54,6 +55,7 @@ void error_at(char *loc, char *fmt, ...);
 void error_tok(Token *tok, char *fmt, ...);
 bool equal(Token *tok, char *op);
 Token *skip(Token *tok, char *op);
+bool consume(Token **rest, Token *tok, char *str);
 Token *tokenize(char *input);
 
 //
@@ -114,12 +116,18 @@ typedef enum {
 
 struct Type {
   TypeKind kind;
+
+  // Pointer
   Type *base;
+
+  // Declaration
+  Token *name;
 };
 
 extern Type *ty_int;
 
 bool is_integer(Type *ty);
+Type *pointer_to(Type *base);
 void add_type(Node *node);
 
 //
